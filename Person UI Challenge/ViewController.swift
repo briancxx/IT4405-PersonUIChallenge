c9740fc06279400bc76c764c9da8df62347b5fb4
Brian Cox <briancxx@icloud.com>
2019-02-15 16:02:32 -0600

    First commit

---
+++ Person UI Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI Challenge
+//
+//  Created by Brian Cox on 2/15/19.
+//  Copyright © 2019 Brian Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
